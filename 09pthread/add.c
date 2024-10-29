10a70acb3e5f4051aa8884145accbd35ea730823
lzc1213Q <1456029025@qq.com>
2024-10-29 17:15:27 +0800

    修改add 编写错误

--- 09pthread/add.c
+++ 09pthread/add.c
@@ -8,34 +8,26 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
-#define THRNUM 20
+#define BUFSIZE 32
+
 static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-static void *fun(void *p)
+
+static void *fun (void *p)
 {
-    int fd;
+    int fd = -1;
     long long n = 0;
-    char buf[THRNUM] = "";
+    char buf[BUFSIZE] = "";
+
     fd = open(p, O_RDWR | O_CREAT, 0664);
-    if (fd < 0)
-    {
-        perror("fopen()");
-        exit(1);
-    }
-    // 临界区
+    /* if err */
+
     pthread_mutex_lock(&mutex);
 
-    //    read(fd, buf, THRNUM);
-    //    lseek(fd, 0, SEEK_SET);
-    //    n = atoi(buf);
-    //    snprintf(buf, THRNUM, "%lld\n", ++n);
-    //    write(fd, buf, strlen(buf));
-    fgets(linebuf, LINESIZE, fp);
-    fseek(fp, 0, SEEK_SET);
-
-    // sleep(1);
-
-    fprintf(fp, "%d\n", atoi(linebuf) + 1); // 全缓冲，需要fflush 或者 fclose 刷新 才能写到目标文件
-    fclose(fp);                             // fprintf(fp  全缓冲，需要fflush 或者 fclose 刷新 才能写到目标文件，所以也在临界区
+    read(fd, buf, BUFSIZE);
+    lseek(fd, 0, SEEK_SET);
+    n = atoll(buf);
+    snprintf(buf, BUFSIZE, "%lld\n", ++n);
+    write(fd, buf, strlen(buf));
 
     close(fd);
 
@@ -44,31 +36,26 @@ static void *fun(void *p)
     pthread_exit(NULL);
 }
 
-int main(int argc, char **argv)
+int main (int argc, char **argv)
 {
-    pthread_t tid[20];
+    pthread_t tids[20];
     int i = 0;
-    int err;
-    if (argc < 2)
-    {
-        fprintf(stderr, "Usage %s <filename\n>", argv[0]);
-        exit(1);
-    }
-    for (i = 0; i < THRNUM; i++)
-    {
-        err = pthread_create(&tid[i], NULL, fun, argv[1]);
-        if (err)
-        {
-            fprintf(stderr, "pthread_create(): %s\n", strerror(err));
-            exit(1);
-        }
-    }
-    for (i = 0; i < THRNUM; i++)
-    {
-        pthread_join(tid[i], NULL);
-    }
-    pthread_mutex_destroy(&mutex);
-    // pthread_mutex_destory(&mutex);
 
-    exit(0);
+    if (argc < 2) {
+        fprintf(stderr, "Usage %s <filename>\n", argv[0]);
+        return -1;
+    }
+
+    for (i = 0; i < 20; i++) {
+        pthread_create(&tids[i], NULL, fun, argv[1]);
+        /* if err */
+    }
+
+    for (i = 0; i < 20; i++) {
+        pthread_join(tids[i], NULL);
+    }
+
+    pthread_mutex_destroy(&mutex);
+
+    return 0;
 }
